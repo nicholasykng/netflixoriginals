e06422fa2f0673bdb106224e2c24324aefe64c09
Nicholas Ng <nicholasykng@gmail.com>
2019-11-11 19:00:42 +0000

    first commit

---
+++ lib/netflixoriginals.rb
@@ -0,0 +1,6 @@
+require "netflixoriginals/version"
+
+module Netflixoriginals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/netflixoriginals/version.rb
@@ -0,0 +1,3 @@
+module Netflixoriginals
+  VERSION = "0.1.0"
+end

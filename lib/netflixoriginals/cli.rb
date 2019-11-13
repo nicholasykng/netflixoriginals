ac3373c375c50c2dda464b4566d1590ab53eb284
Nicholas Ng <nicholasykng@gmail.com>
2019-11-13 05:00:52 +0000

    Updated url

--- lib/netflixoriginals/cli.rb
+++ lib/netflixoriginals/cli.rb
@@ -2,20 +2,22 @@ class Netflixoriginals::CLI
 
   def call
     make_series
-    puts "Welcome to the List of Top Netflix Original Series According to Rotten Tomatoes."
+    puts "Welcome to the List of Top Netflix Original Movies According to Rotten Tomatoes."
+    puts "The list is sorted from the left to right: Movie Rank, Movie Title, Year Movie was Released, and Tomatometer Score"
     start
     goodbye
   end
 
   def make_series
-    series_array = Netflixoriginals::Scraper.scrape_index_page("http://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/6/")
+    series_array = Netflixoriginals::Scraper.scrape_index_page("https://editorial.rottentomatoes.com/guide/best-netflix-movies-to-watch-right-now/")
     Netflixoriginals::Series.create_from_collection(series_array)
   end
 
   def start
     Netflixoriginals::Series.all.each do |series|
-      puts "#{series.rank}. #{series.url}"
+      puts "#{series.rank}. #{series.title} #{series.year} #{series.tomatometer}"
     end
+    puts "To find out more information about a series please enter the corresponding rank number:"
   end
 
 
